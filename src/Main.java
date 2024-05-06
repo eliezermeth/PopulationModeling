9b0c037cc9c76921247b1fa570066a9bc5e9a4a7
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2024-05-06 17:04:03 -0400

    Changed int to BigInteger; results look better.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static java.math.BigInteger.*;
 
 public class Main
 {
@@ -12,7 +15,7 @@ public class Main
 
     public final int YEARS_TO_RUN = 210;
 
-    public ArrayList<int[]> popPool = new ArrayList();
+    public ArrayList<BigInteger[]> popPool = new ArrayList();
 
     // age pool layout:
     // [unmarried male, unmarried female, married male, married female]
@@ -22,9 +25,9 @@ public class Main
         populateInitialPools(20);
 
         // add 50 singles; to become 25 breeding pairs
-        int[] temp = popPool.get(19); // age 19; becomes 20 at year cycle
-        temp[0] = 25;
-        temp[1] = 25;
+        BigInteger[] temp = popPool.get(19); // age 19; becomes 20 at year cycle
+        temp[0] = valueOf(25);
+        temp[1] = valueOf(25);
 
         for (int i = 0; i < YEARS_TO_RUN; i++)
         {
@@ -40,7 +43,7 @@ public class Main
     public void yearCycle()
     {
         // add year (cause everyone to age up as well)
-        popPool.add(0, new int[]{0, 0, 0, 0});
+        popPool.add(0, new BigInteger[]{ZERO, ZERO, ZERO, ZERO});
 
         // kill everyone beyond maximum age
         if (popPool.size() - 1 == MAX_AGE + 1)
@@ -52,7 +55,7 @@ public class Main
         calculateMarriages();
 
         // calculate childbirths
-        int[] newChildren = calculateChildbirth();
+        BigInteger[] newChildren = calculateChildbirth();
         popPool.get(0)[0] = newChildren[0];
         popPool.get(0)[1] = newChildren[1];
     }
@@ -60,18 +63,18 @@ public class Main
     /**
      * Calculate all childbirths in the year.
      */
-    public int[] calculateChildbirth()
+    public BigInteger[] calculateChildbirth()
     {
-        int[] newChildren = {0, 0};
+        BigInteger[] newChildren = {ZERO, ZERO};
 
         int end = popPool.size() - 1;
         if (end > END_CHILDBIRTH_YEARS) end = END_CHILDBIRTH_YEARS - 1;
         for (int i = end; i >= BEGIN_CHILDBIRTH_YEARS; i--) // calculate births from oldest to youngest
         {
-            int[] temp = calculateChildbirthUtil(i);
+            BigInteger[] temp = calculateChildbirthUtil(i);
 
-            newChildren[0] += temp[0];
-            newChildren[1] += temp[1];
+            newChildren[0] = newChildren[0].add(temp[0]);
+            newChildren[1] = newChildren[1].add(temp[1]);
         }
 
         return newChildren;
@@ -80,22 +83,22 @@ public class Main
     /**
      * Calculate childbirths for a single age pool in a year.
      */
-    public int[] calculateChildbirthUtil(int age)
+    public BigInteger[] calculateChildbirthUtil(int age)
     {
         // TODO chance of childbirth
         // TODO chance of twin, triplets, etc
         // TODO chance of childbirth deaths
 
-        int[] newChildren = {0, 0};
-        for (int i = 0; i < popPool.get(age)[3]; i++) // for each married female
+        BigInteger[] newChildren = {ZERO, ZERO};
+        for (BigInteger i = ZERO; i.compareTo(popPool.get(age)[3]) < 0; i = i.add(ONE)) // for each married female
         {
             if (Math.random() < FERTILITY_RATE) // give birth
             {
                 // currently only single births; 50/50 male/female
                 if (Math.random() < MALE_CHANCE)
-                    newChildren[0] += 1; // male
+                    newChildren[0] = newChildren[0].add(ONE); // male
                 else
-                    newChildren[1] += 1; // female
+                    newChildren[1] = newChildren[1].add(ONE); // female
             }
         }
 
@@ -104,51 +107,57 @@ public class Main
 
     public void calculateMarriages()
     {
-        // get highest marriagable age
+        // get highest marriageable age
         int end = popPool.size() - 1;
         if (end > END_CHILDBIRTH_YEARS) end = END_CHILDBIRTH_YEARS - 1;
 
         for (int age = end; age > BEGIN_CHILDBIRTH_YEARS - 1; age--)
         {
-            int[] currentAgePool = popPool.get(age);
+            BigInteger[] currentAgePool = popPool.get(age);
             // attempt same age batch marriages
-            if (currentAgePool[0] != 0 && currentAgePool[1] != 0)
+            if (!currentAgePool[0].equals(ZERO) && !currentAgePool[1].equals(ZERO))
             {
                 // get lower number in age, males or females
-                int num = Math.min(currentAgePool[0], currentAgePool[1]);
+                BigInteger num = currentAgePool[0].min(currentAgePool[1]);
 
                 // subtract that number from singles and add to marriages
-                currentAgePool[0] -= num; currentAgePool[2] += num; // male
-                currentAgePool[1] -= num; currentAgePool[3] += num; // female
+                currentAgePool[0] = currentAgePool[0].subtract(num); // male
+                currentAgePool[2] = currentAgePool[2].add(num); // male
+                currentAgePool[1] = currentAgePool[1].subtract(num); // female
+                currentAgePool[3] = currentAgePool[3].add(num); // female
             }
             // at this point, either no single males or no single females (or both) remaining
-            if (currentAgePool[0] != 0) // males remaining
+            if (!currentAgePool[0].equals(ZERO)) // males remaining
             {
                 for (int i = 1; i <= MAX_MARRIAGE_GAP && age - i >= BEGIN_CHILDBIRTH_YEARS // within 5 years; least 20 years old
-                        && currentAgePool[0] != 0; i++) // and single males remaining
+                        && !currentAgePool[0].equals(ZERO); i++) // and single males remaining
                 {
-                    if (popPool.get(age - i)[1] != 0) // available in age pool
+                    if (!popPool.get(age - i)[1].equals(ZERO)) // available in age pool
                     {
-                        int num = Math.min(currentAgePool[0], popPool.get(age - i)[1]);
+                        BigInteger num = currentAgePool[0].min(popPool.get(age - i)[1]);
 
                         // subtract that number from singles and add to marriages
-                        currentAgePool[0] -= num; currentAgePool[2] += num; // male
-                        popPool.get(age - i)[1] -= num; popPool.get(age - i)[3] += num; // female
+                        currentAgePool[0] = currentAgePool[0].subtract(num); // male
+                        currentAgePool[2] = currentAgePool[2].add(num); // male
+                        popPool.get(age - i)[1] = popPool.get(age - 1)[1].subtract(num); // female
+                        popPool.get(age - i)[3] = popPool.get(age - 1)[3].add(num); // female
                     }
                 }
             }
-            if (currentAgePool[1] != 0) // females remaining
+            if (!currentAgePool[1].equals(ZERO)) // females remaining
             {
                 for (int i = 1; i <= MAX_MARRIAGE_GAP && age - i >= BEGIN_CHILDBIRTH_YEARS // within 5 years; least 20 years old
-                        && currentAgePool[1] != 0; i++) // and single females remaining
+                        && !currentAgePool[1].equals(ZERO); i++) // and single females remaining
                 {
-                    if (popPool.get(age - i)[0] != 0) // available in age pool
+                    if (!popPool.get(age - i)[0].equals(ZERO)) // available in age pool
                     {
-                        int num = Math.min(currentAgePool[1], popPool.get(age - i)[0]);
+                        BigInteger num = currentAgePool[1].min(popPool.get(age - i)[0]);
 
                         // subtract that number from singles and add to marriages
-                        currentAgePool[1] -= num; currentAgePool[3] += num; // female
-                        popPool.get(age - i)[0] -= num; popPool.get(age - i)[2] += num; // male
+                        currentAgePool[1] = currentAgePool[1].subtract(num); // female
+                        currentAgePool[3] = currentAgePool[3].add(num); // female
+                        popPool.get(age - i)[0] = popPool.get(age - 1)[0].subtract(num); // male
+                        popPool.get(age - i)[2] = popPool.get(age - 1)[2].add(num); // male
                     }
                 }
             }
@@ -159,23 +168,23 @@ public class Main
     public void populateInitialPools(int numGenerations)
     {
         for (int i = 0; i < numGenerations; i++)
-            popPool.add(0, new int[]{0, 0, 0, 0});
+            popPool.add(0, new BigInteger[]{ZERO, ZERO, ZERO, ZERO});
     }
 
-    public int getPopulationOfAge(int age)
+    public BigInteger getPopulationOfAge(int age)
     {
-        int sum = 0;
-        int[] array = popPool.get(age);
+        BigInteger sum = ZERO;
+        BigInteger[] array = popPool.get(age);
         for (int i = 0; i < array.length; i++)
-            sum += array[i];
+            sum = sum.add(array[i]);
         return sum;
     }
 
-    public int getTotalPopulation()
+    public BigInteger getTotalPopulation()
     {
-        int total = 0;
+        BigInteger total = ZERO;
         for (int i = 0; i < popPool.size(); i++)
-            total += getPopulationOfAge(i);
+            total = total.add(getPopulationOfAge(i));
         return total;
     }
 
