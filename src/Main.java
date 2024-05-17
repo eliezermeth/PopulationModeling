63dce4956a35117f09722cc550e8fb823f7c041c
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2024-05-17 16:09:58 -0400

    Changed long to double; allows larger population.

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 
 public class Main
 {
-    public final int FERTILITY_RATE = 1; // chance of childbirth, 0-1
+    public final double FERTILITY_RATE = 1; // chance of childbirth, 0-1
     public final double MALE_CHANCE = 0.5; // chance of male birth out of 1
     public final int BEGIN_CHILDBIRTH_YEARS = 20;
-    public final int END_CHILDBIRTH_YEARS = 60; // no longer able to give birth
+    public final int END_CHILDBIRTH_YEARS = 40; // no longer able to give birth
     public final int MAX_AGE = 120; // everyone over this age (next year) automatically dies
     public final int MAX_MARRIAGE_GAP = 5; // maximum difference in age between partners in marriage
 
     public final int YEARS_TO_RUN = 210;
 
-    public ArrayList<int[]> popPool = new ArrayList();
+    public ArrayList<double[]> popPool = new ArrayList();
 
     // age pool layout:
     // [unmarried male, unmarried female, married male, married female]
@@ -22,7 +22,7 @@ public class Main
         populateInitialPools(20);
 
         // add 50 singles; to become 25 breeding pairs
-        int[] temp = popPool.get(19); // age 19; becomes 20 at year cycle
+        double[] temp = popPool.get(19); // age 19; becomes 20 at year cycle
         temp[0] = 25;
         temp[1] = 25;
 
@@ -40,7 +40,7 @@ public class Main
     public void yearCycle()
     {
         // add year (cause everyone to age up as well)
-        popPool.add(0, new int[]{0, 0, 0, 0});
+        popPool.add(0, new double[]{0, 0, 0, 0});
 
         // kill everyone beyond maximum age
         if (popPool.size() - 1 == MAX_AGE + 1)
@@ -52,7 +52,7 @@ public class Main
         calculateMarriages();
 
         // calculate childbirths
-        int[] newChildren = calculateChildbirth();
+        double[] newChildren = calculateChildbirth();
         popPool.get(0)[0] = newChildren[0];
         popPool.get(0)[1] = newChildren[1];
     }
@@ -60,15 +60,15 @@ public class Main
     /**
      * Calculate all childbirths in the year.
      */
-    public int[] calculateChildbirth()
+    public double[] calculateChildbirth()
     {
-        int[] newChildren = {0, 0};
+        double[] newChildren = {0, 0};
 
         int end = popPool.size() - 1;
         if (end > END_CHILDBIRTH_YEARS) end = END_CHILDBIRTH_YEARS - 1;
         for (int i = end; i >= BEGIN_CHILDBIRTH_YEARS; i--) // calculate births from oldest to youngest
         {
-            int[] temp = calculateChildbirthUtil(i);
+            double[] temp = calculateChildbirthUtil(i);
 
             newChildren[0] += temp[0];
             newChildren[1] += temp[1];
@@ -80,14 +80,14 @@ public class Main
     /**
      * Calculate childbirths for a single age pool in a year.
      */
-    public int[] calculateChildbirthUtil(int age)
+    public double[] calculateChildbirthUtil(int age)
     {
         // TODO chance of childbirth
         // TODO chance of twin, triplets, etc
         // TODO chance of childbirth deaths
 
-        int[] newChildren = {0, 0};
-        for (int i = 0; i < popPool.get(age)[3]; i++) // for each married female
+        double[] newChildren = {0, 0};
+        for (double i = 0; i < popPool.get(age)[3]; i++) // for each married female
         {
             if (Math.random() < FERTILITY_RATE) // give birth
             {
@@ -104,18 +104,18 @@ public class Main
 
     public void calculateMarriages()
     {
-        // get highest marriagable age
+        // get highest marriageable age
         int end = popPool.size() - 1;
         if (end > END_CHILDBIRTH_YEARS) end = END_CHILDBIRTH_YEARS - 1;
 
         for (int age = end; age > BEGIN_CHILDBIRTH_YEARS - 1; age--)
         {
-            int[] currentAgePool = popPool.get(age);
+            double[] currentAgePool = popPool.get(age);
             // attempt same age batch marriages
             if (currentAgePool[0] != 0 && currentAgePool[1] != 0)
             {
                 // get lower number in age, males or females
-                int num = Math.min(currentAgePool[0], currentAgePool[1]);
+                double num = Math.min(currentAgePool[0], currentAgePool[1]);
 
                 // subtract that number from singles and add to marriages
                 currentAgePool[0] -= num; currentAgePool[2] += num; // male
@@ -129,7 +129,7 @@ public class Main
                 {
                     if (popPool.get(age - i)[1] != 0) // available in age pool
                     {
-                        int num = Math.min(currentAgePool[0], popPool.get(age - i)[1]);
+                        double num = Math.min(currentAgePool[0], popPool.get(age - i)[1]);
 
                         // subtract that number from singles and add to marriages
                         currentAgePool[0] -= num; currentAgePool[2] += num; // male
@@ -144,7 +144,7 @@ public class Main
                 {
                     if (popPool.get(age - i)[0] != 0) // available in age pool
                     {
-                        int num = Math.min(currentAgePool[1], popPool.get(age - i)[0]);
+                        double num = Math.min(currentAgePool[1], popPool.get(age - i)[0]);
 
                         // subtract that number from singles and add to marriages
                         currentAgePool[1] -= num; currentAgePool[3] += num; // female
@@ -159,21 +159,21 @@ public class Main
     public void populateInitialPools(int numGenerations)
     {
         for (int i = 0; i < numGenerations; i++)
-            popPool.add(0, new int[]{0, 0, 0, 0});
+            popPool.add(0, new double[]{0, 0, 0, 0});
     }
 
-    public int getPopulationOfAge(int age)
+    public double getPopulationOfAge(int age)
     {
-        int sum = 0;
-        int[] array = popPool.get(age);
+        double sum = 0;
+        double[] array = popPool.get(age);
         for (int i = 0; i < array.length; i++)
             sum += array[i];
         return sum;
     }
 
-    public int getTotalPopulation()
+    public double getTotalPopulation()
     {
-        int total = 0;
+        double total = 0;
         for (int i = 0; i < popPool.size(); i++)
             total += getPopulationOfAge(i);
         return total;
@@ -182,7 +182,7 @@ public class Main
     public void displayPools()
     {
         for (int i = 0; i < popPool.size(); i++)
-            System.out.println(String.format("%2d", i) + " : " + Arrays.toString(popPool.get(i)));
+            System.out.println(String.format("%3d", i) + " : " + Arrays.toString(popPool.get(i)));
     }
 
     public static void main(String[] args)
